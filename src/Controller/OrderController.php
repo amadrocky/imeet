008eba5fe8094cd225530b3fce8b7378ce275788
amadrocky <amadou.kane.dev@gmail.com>
2023-11-01 17:18:47 +0100

    Adding stepper and order recap

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/order', name: 'app_order')]
+class OrderController extends AbstractController
+{
+    #[Route('/', name: '_recap')]
+    public function index(): Response
+    {
+        return $this->render('order/index.html.twig', [
+            
+        ]);
+    }
+}
