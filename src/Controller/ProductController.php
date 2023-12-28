5bce9836f5128118bfeb271a7298fd1a59eeb7c7
amadrocky <ak45amad@hotmail.fr>
2023-12-28 11:39:29 +0100

    Products dynamic data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,19 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/products', name: 'app_product')]
 class ProductController extends AbstractController
 {
+    public function __construct(
+        private ProductRepository $productRepository
+    ) {
+    }
+
     #[Route('/', name: '_index')]
     public function index(): Response
     {
         return $this->render('product/index.html.twig', [
-            
+            'products' => $this->productRepository->findBy(['enabled' => true])
         ]);
     }
 
-    #[Route('/show', name: '_show')]
-    public function show(): Response
+    #[Route('/show/{slug}', name: '_show')]
+    public function show(Product $product): Response
     {
         return $this->render('product/show.html.twig', [
-            
+            'product' => $product
         ]);
     }
 }
