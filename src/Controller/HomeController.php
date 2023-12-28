c754b3967f7bf8768c11abbc8878f606d94b0e92
amadrocky <ak45amad@hotmail.fr>
2023-12-28 01:48:33 +0100

    Dynamic product card

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\CompositionRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private ProductRepository $productRepository,
+        private CompositionRepository $compositionRepository
+    ) {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $this->productRepository->findBy(['enabled' => true]),
+            'compositions' => $this->compositionRepository->findAll()
         ]);
     }
 }
