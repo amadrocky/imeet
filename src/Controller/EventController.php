6baed780e1a2821d5b808ba9008503caa70fb0f8
amadrocky <ak45amad@hotmail.fr>
2024-02-11 22:49:00 +0100

    Export PDF (static front)

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\User;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,15 +19,43 @@ class EventController extends AbstractController
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
 
-        /** @var User $user */
-        $user = $this->getUser();
-
-        if ($user->getEmail() !== $event->getEmail()) {
-            throw new Exception('The actual user doesn\'t own this event.');
-        }
+        $this->isOwner($this->getUser(), $event);
 
         return $this->render('event/scan.html.twig', [
             'event' => $event
         ]);
+    }
+
+    #[Route('/event/{id}/tickets', name: '_tickets')]
+    public function createAndExportTickets(Event $event)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+
+        $this->isOwner($this->getUser(), $event);
+
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->set('isRemoteEnabled', true);
+
+        $dompdf = new Dompdf($pdfOptions);
+        
+        $html = $this->renderView('PDF/tickets.html.twig', [
+            'event' => $event,
+            'tickets' => $event->getTickets()
+        ]);
+
+        $dompdf->loadHtml(($html));
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream(ucfirst($event->getName()) . '.pdf', [
+            "Attachment" => false
+        ]);
+    }
+
+    public function isOwner(User $user, Event $event): void
+    {
+        if ($user->getEmail() !== $event->getEmail()) {
+            throw new Exception('The actual user doesn\'t own this event.');
+        }
     }
 }
