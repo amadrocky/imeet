91cc22540f6e0d3eebfc67589f5928d3b3016ea3
amadrocky <ak45amad@hotmail.fr>
2024-04-24 19:53:02 +0200

    Replace annotations to attributes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,7 +6,7 @@ use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/products', name: 'app_product')]
 class ProductController extends AbstractController
